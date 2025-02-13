1e7a3f7156acb97f0e4cf7f1b9ee1be00eaeb2e8
JoeSteeloak <141022479+JoeSteeloak@users.noreply.github.com>
2025-02-13 11:35:37 +0100

    add database connection

---
+++ Migrations/20250213103517_InitialCreate.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EFC.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Books",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Title = table.Column<string>(type: "TEXT", nullable: false),
+                    Author = table.Column<string>(type: "TEXT", nullable: false),
+                    Genre = table.Column<string>(type: "TEXT", nullable: true),
+                    Pages = table.Column<int>(type: "INTEGER", nullable: true),
+                    Finished = table.Column<bool>(type: "INTEGER", nullable: false),
+                    Loaned = table.Column<bool>(type: "INTEGER", nullable: false),
+                    Loaner = table.Column<string>(type: "TEXT", nullable: true),
+                    LoanDate = table.Column<DateOnly>(type: "TEXT", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Books", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Books");
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,7 +1,12 @@
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<EFC.Data.BookDbContext>(options =>
+    options.UseSqlite(builder.Configuration.GetConnectionString("DefaultDbConnection"))
+);
 
 var app = builder.Build();
 
